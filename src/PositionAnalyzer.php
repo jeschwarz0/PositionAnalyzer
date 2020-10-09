5dc95eef709f01f63f8f877bec1a7f7a4a764e88
Jesse Schwarz <jesseschwarz0@gmail.com>
2020-10-08 19:24:29 -0500

    Added documentation to PositionAnalyzer
     - Added braces
     - Added PHPDoc comments
     - Added Netbeans folder to .gitignore

--- src/PositionAnalyzer.php
+++ src/PositionAnalyzer.php
@@ -4,34 +4,76 @@ class PositionAnalyzer
 {
     protected $_config;
 
+    /**
+     * 
+     * Constructs the class.
+     * Instantiates configuration object.
+     */
     public function __construct($configPath)
     {
         $this->_config = FALSE;
-        if (is_string($configPath) && file_exists($configPath))
+        if (is_string($configPath) && file_exists($configPath)){
             $fileContents = file_get_contents($configPath);
             if (isset($fileContents) && $fileContents !== FALSE){
                 $this->_config = simplexml_load_string($fileContents);
                 $this->buildPercentTables();
             }
+        }
     }
 
+    /**
+     * 
+     * Destructs the class.
+     */
     public function __destruct()
     {
         unset($this->_config);
     }
 
+    /**
+     * 
+     * Checks if the base configuration is valid.
+     * @return bool True if configuration is loaded and valid.
+     */
     public function isValid()
     {
         return $this->_config !== FALSE;
     }
 
+    /**
+     * 
+     * Processes a JobApis Job object and generates analysis information based off it.
+     * Format: 
+     *   [CategoryName] => Array
+     *       (
+     *           [entries] => Array
+     *               (
+     *                   [EntryName] => Array
+     *                       (
+     *                           [score] => 
+     *                           [is_match] => 
+     *                       )
+     *               )
+     *           [sum] => 
+     *           [pct] => 
+     *           [any_match] => 
+     *           [title_match] => 
+     *           [is_global] => SimpleXMLElement Object
+     *               (
+     *                   [0] => 
+     *               )
+     * 
+     * @param \JobApis\Jobs\Client\Job $position The position to be analyzed.
+     * @return array|FALSE Returns an array with summary results or FALSE on failure.
+     */
     public function analyzePositionToArray(&$position)
     {
-        if (!$this->isValid()) return FALSE;
+        if (!$this->isValid()) {return FALSE;}
         $config_version = intval($this->_config->ConfigVersion);
-        if ($config_version > 2) return FALSE;
-        $result = array();
+        if ($config_version > 2) {return FALSE;}// Do not continue if version is > 2
+        $result = array();// Initialize result
         foreach ($this->_config->SearchCategory as $categoryArr) {
+            //Initialize entries
             $result[(string) $categoryArr->Name] = array();
             $result[(string) $categoryArr->Name]['entries'] = array();
             foreach ($categoryArr->CategoryValue as $categoryVal) {
@@ -55,6 +97,7 @@ class PositionAnalyzer
                     $result[(string) $categoryArr->Name]['entries'][(string) $categoryVal->EntryName] = $entryRec;
                 }
             }
+            //Generate sum of values
             $sum = array_sum(array_column($result[(string) $categoryArr->Name]['entries'], 'score'));
             $result[(string) $categoryArr->Name]['sum'] = $sum;
             $result[(string) $categoryArr->Name]['pct'] = PositionAnalyzer::calculatePercentage($sum, $categoryArr['min'], $categoryArr['max']);
@@ -64,9 +107,8 @@ class PositionAnalyzer
                 if (isset($categoryArr->CategoryTitle)) {
                     for ($titleIdx = 0;!$title_match && $titleIdx < $categoryArr->CategoryTitle->Term->count(); $titleIdx++) {
                         if (stripos($position->title, (string) $categoryArr->CategoryTitle->Term[$titleIdx]) !== false) {
-                            $title_match = true;
+                            $title_match = true;//Set $title_match if there is a match
                         }
-
                     }
                 }
                 $result[(string) $categoryArr->Name]['title_match'] = $title_match;
@@ -76,12 +118,17 @@ class PositionAnalyzer
         return $result;
     }
 
+    /**
+     * 
+     * Appends percentage summary information to configuration object.
+     * Calculates the min and max scores of every CategoryValue.
+     */
     private function buildPercentTables()
     {
-        if (!$this->isValid() || intval($this->_config->ConfigVersion) <= 0) return false;
+        if (!$this->isValid() || intval($this->_config->ConfigVersion) <= 0) {return false;}
         foreach ($this->_config->SearchCategory as $categoryArr) {
             $catmin = 0;
-            $catmax = 0;
+            $catmax = 0;// For each CategoryValue add the min and max attributes
             foreach ($categoryArr->CategoryValue as $categoryVal) {
                 $catmin += min(intval($categoryVal->MatchValue), intval($categoryVal->NonMatchValue));
                 $catmax += max(intval($categoryVal->MatchValue), intval($categoryVal->NonMatchValue));
@@ -91,6 +138,15 @@ class PositionAnalyzer
         }
     }
 
+    /**
+     * 
+     * Calculates a percentage ranging from 0 to max or min.
+     * Negative values are possible.
+     * @param int $sum The value to calculate
+     * @param int $min The minimum possible value
+     * @param int $max The maximum possible value
+     * @return int The percentage (positive or negative) rounded.
+     */
     private static function calculatePercentage(&$sum, $min, $max)
     {
         $divisor = intval($sum > 0 ? $max : $min);
@@ -121,21 +177,29 @@ class PositionAnalyzer
     { 
         $html = $tabs = '';
         // Build tab prefix of $tabCount
-        if (is_int($tabCount))
-            for ($tabidx = 0; $tabidx < $tabCount; $tabidx++)
+        if (is_int($tabCount)){
+            for ($tabidx = 0; $tabidx < $tabCount; $tabidx++){
                 $tabs .= "\t";
-        else $tabs = '';
+            }
+        }else {$tabs = '';}
         if (is_array($input)){
             $html .= "$tabs<ul>" . PHP_EOL;
             foreach ($input as $categoryKey => $categoryValue){
                 if ($categoryValue['any_match'] === true){
                     $verbose_summary = 'title="' . $categoryValue['sum'] . ': ';
                     foreach($categoryValue['entries'] as $entryKey => $entryValue){
-                        if ($entryValue['score'] !== 0)
-                            $verbose_summary .= htmlspecialchars($entryKey) . "(" . $entryValue['score'] . ($entryValue['is_match'] ? 'm' : 'n') . ") ";
-                    }
+                        if ($entryValue['score'] !== 0){
+                            $verbose_summary .= htmlspecialchars($entryKey) . "(" . 
+                                    $entryValue['score'] . ($entryValue['is_match'] ? 'm' : 'n') . ") ";
+                        }    
+                        }
                     $verbose_summary .= '"';
-                    $html.= "$tabs\t<li $verbose_summary" . ' class="' . ($categoryValue['pct'] >= 0 ? 'pa-catmatch' : 'pa-catmismatch') . ($categoryValue['title_match'] ? ' pa-titlematch' : '') . ($categoryValue['is_global'] ? ' pa-globalcat' : '') . '">' . htmlspecialchars($categoryKey) . " : " . $categoryValue['pct'] . "%</li>" . PHP_EOL;
+                    $html.= "$tabs\t<li $verbose_summary" . ' class="' . 
+                            ($categoryValue['pct'] >= 0 ? 'pa-catmatch' : 'pa-catmismatch') . 
+                            ($categoryValue['title_match'] ? ' pa-titlematch' : '') . 
+                            ($categoryValue['is_global'] ? ' pa-globalcat' : '') . '">' . 
+                            htmlspecialchars($categoryKey) . " : " . 
+                            $categoryValue['pct'] . "%</li>" . PHP_EOL;
                 }
             }
             $html .= "$tabs</ul>" . PHP_EOL;
